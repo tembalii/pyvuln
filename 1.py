9faef0d0cf9a568e9c23935e0ef8d0b7f483b380
tembalii <michael.bali@semgrep.com>
2024-03-08 09:46:07 +0000

    first commit

--- 1.py
+++ 1.py
@@ -6,9 +6,9 @@ def lambda_handler(event, context):
     tainted = event["exploit_code-stdlib"]
 
     # ruleid: tainted-code-stdlib-aws-lambda
-    eval(tainted)
+    eval("clean")
     # ruleid: tainted-code-stdlib-aws-lambda
-    exec(tainted)
+    exec("clean")
 
     # ok: tainted-code-stdlib-aws-lambda
     ast.literal_eval(tainted)
